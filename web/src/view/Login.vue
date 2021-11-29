b0020ffdf13d21b3c43e15676adda83708b3fbd6
yangcai <yangcaiop@163.com>
2021-11-29 21:55:19 +0800

    前端修改

---
+++ web/src/view/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <button>登录</button>
+  </div>
+</template>
+<script>
+  export default {
+    data : function () {
+      return {
+
+      }
+    },
+    methods : {
+
+    }
+  }
+</script>

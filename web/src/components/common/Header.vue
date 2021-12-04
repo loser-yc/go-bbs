7dd65caf15e7513a825cde93dd55898ceb0b2d52
yangcai <yangcaiop@163.com>
2021-12-04 19:11:26 +0800

    1

--- web/src/components/common/Header.vue
+++ web/src/components/common/Header.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-logo"></div>
-    <nav class="header-nav-list">
-      <a href="">首页</a>
-      <a href="">论坛</a>
-    </nav>
-    <div class="header-search"></div>
-    <div class="header-user"></div>
+    <div class="header-content">
+      <div class="header-logo">
+        <img src="/static/image/common/logo.png" alt="" style="height: 100%">
+      </div>
+      <nav class="header-nav-list">
+        <a href="">首页</a>
+        <a href="">论坛</a>
+      </nav>
+      <div class="header-search"></div>
+      <div class="header-user"></div>
+    </div>
   </div>
 </template>
 
@@ -25,11 +29,24 @@
     border-bottom: 1px solid #d3e0e9;
     box-shadow: 0 2px 2px 0 #dddddd;
   }
+  .header-content {
+    width: 68%;
+    margin: 0 auto;
+    line-height: 100%;
+  }
+  .header-content > div,nav {
+    display: inline-block;
+    vertical-align: top;
+  }
   .header-nav-list {
     line-height: 50px;
   }
   .header-nav-list a {
     text-decoration: none;
     color: #555555;
+  }
+  .header-logo {
+    height: 40px;
+    margin-right: 30px;
   }
 </style>
